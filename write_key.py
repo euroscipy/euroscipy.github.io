82c97e93e01b5d9a30d417115d097a7e2e692a7c
Pierre de Buyl <pdebuyl@pdebuyl.be>
2020-01-20 21:27:14 +0100

    ssh more

--- write_key.py
+++ write_key.py
@@ -1,8 +1,9 @@
 import os
+import os.path
 
 with open('id_ssh', 'w') as f:
     print(os.environ['SSH_KEY'], file=f)
 
-with open('.ssh/known_hosts', 'w') as f:
+with open(os.path.expanduser('~/.ssh/known_hosts'), 'w') as f:
     print(os.environ['SSH_KNOWN_HOST'], file=f)
 
