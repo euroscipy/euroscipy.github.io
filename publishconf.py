c308fb9b58b6acbef0e0d62b5eae1f5e61040036
Pierre de Buyl <pdebuyl@pdebuyl.be>
2020-01-29 10:27:38 +0100

    use RELATIVE_URLS=True
    The published version produced incorrect links for css etc, such as
    "./2020/www.euroscipy.org/theme_2020/..".

--- publishconf.py
+++ publishconf.py
@@ -6,7 +6,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = os.environ.get('SITEURL', 'www.euroscipy.org')
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
